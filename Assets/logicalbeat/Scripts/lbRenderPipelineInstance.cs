2ffc964def07366e3f1540de8e5e549a56a9ed0e
Yoshiki Domae <yoshiki.domae@logicalbeat.jp>
2019-09-21 02:33:27 +0900

    最低限のRenderPipelineを用意。

---
+++ Assets/logicalbeat/Scripts/lbRenderPipelineInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+#if	UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class lbRenderPipelineInstance : RenderPipeline
+{
+	// コンストラクタ
+	public lbRenderPipelineInstance( lbRenderPipelineAsset asset )
+	{
+	}
+
+	// 描画処理
+	protected override void Render( ScriptableRenderContext context, Camera[] cameras )
+	{
+		// カメラごとに処理
+		foreach ( var camera in cameras )
+		{
+		}
+
+		// コンテキストのサブミット
+		context.Submit();
+	}
+}
