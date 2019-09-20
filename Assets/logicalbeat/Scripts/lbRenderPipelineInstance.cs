59ab71f669da3e4cbd39894ff1fea526e2f750c2
Yoshiki Domae <yoshiki.domae@logicalbeat.jp>
2019-09-21 05:36:30 +0900

    シンプルなSRPを追加。

--- Assets/logicalbeat/Scripts/lbRenderPipelineInstance.cs
+++ Assets/logicalbeat/Scripts/lbRenderPipelineInstance.cs
@@ -20,6 +20,25 @@ public partial class lbRenderPipelineInstance : RenderPipeline
 		// カメラごとに処理
 		foreach ( var camera in cameras )
 		{
+			// カメラプロパティ設定
+			context.SetupCameraProperties( camera );
+
+			// カリング処理
+			CullingResults				cullResults = new CullingResults();
+			ScriptableCullingParameters	cullingParameters;
+			if ( !camera.TryGetCullingParameters( false, out cullingParameters ) ) continue;
+			cullResults = context.Cull( ref cullingParameters );
+
+			// フィルタリング＆ソート設定
+			SortingSettings sortingSettings = new SortingSettings( camera ) { criteria = SortingCriteria.CommonOpaque };
+			var	settings = new DrawingSettings( new ShaderTagId( "lbDeferred" ), sortingSettings );		// LightMode = lbDeferredのところを描く
+			var	filterSettings = new FilteringSettings(
+									new RenderQueueRange( 0, (int)RenderQueue.GeometryLast ),			// Queue = 0～3000まで
+									camera.cullingMask
+								);
+
+			// 描画処理
+			context.DrawRenderers( cullResults, ref settings, ref filterSettings );
 		}
 
 		// コンテキストのサブミット
